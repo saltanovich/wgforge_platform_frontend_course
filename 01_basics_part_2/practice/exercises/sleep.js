ba21ca6776457eb2e7fef6618950c840f5b16c6b
Alex <i_am_good@tut.by>
2019-04-02 17:53:40 +0300

    Smoosh, sleep implementation

--- 01_basics_part_2/practice/exercises/sleep.js
+++ 01_basics_part_2/practice/exercises/sleep.js
@@ -5,6 +5,11 @@
  * выполнение и вернуть undefined.
  */
 
-export default function sleep() {
-  // your code here
+export default function sleep(time) {
+  const delayTime = time * 1000;
+  if (!Number.isInteger(time) || isNaN(delayTime) || time < 0) {
+    return;
+  }
+  const now = new Date().getTime();
+  while (now + delayTime > new Date().getTime()) {}
 }
