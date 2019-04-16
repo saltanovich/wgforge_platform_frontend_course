12ace53c688b35450b9f33ec36386ac207ee2d2e
saltanovich <i_am_good@tut.by>
2019-04-16 07:18:08 +0300

    cloneDeep

--- 03_iterators/practice/exercises/cloneDeep.js
+++ 03_iterators/practice/exercises/cloneDeep.js
@@ -1,6 +1,14 @@
 /**
  * Реализовать функцию cloneDeep которая копирует объекты по значению с любой глубиной вложенности
  */
-export default function cloneDeep() {
-  // ¯\_(ツ)_/¯
+export default function cloneDeep(obj) {
+  const cloneObj = {};
+  for (let i of Object.keys(obj)) {
+    if (typeof obj[i] === 'object' && obj[i] !== null) {
+      cloneObj[i] = cloneDeep(obj[i]);
+    } else {
+      cloneObj[i] = obj[i];
+    }
+  }
+  return cloneObj;
 }
